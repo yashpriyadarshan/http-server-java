1de6e420fd55bc49cfaf2d902ed7932e3d4b5ab5
Yash Priyadarshan <yashpriyadarshan@gmail.com>
2025-03-17 23:55:56 +0530

    Extracting Url Path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,14 +16,29 @@ public class Main {
   
       clientSocket = serverSocket.accept(); // Wait for connection from client.
       
-      System.out.println("accepted new connection");
-      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-
-      serverSocket.close();
-      clientSocket.close();
-    
+      System.out.println("accepted new connection");    
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
+    }
+
+    BufferedReader clientIn = null;
+    OutputStream clientOut = null;
+    String input = null;
+    try {
+      clientIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      clientOut = clientSocket.getOutputStream();
+
+      input = clientIn.readLine();
+      String get[] = input.split(" ", 0);
+
+      if(get[1].equals("/")) {
+        clientOut.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      } else {
+        clientOut.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+      }
+      
+    } catch (IOException e) {
+      System.out.println("IOException " + e.getMessage());
     }
   }
 }
